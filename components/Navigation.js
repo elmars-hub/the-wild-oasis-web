9ac0d3ae3d60d25a2a2363c924a98d63c4b0a1e6
Martin Elmars <121293732+elmars-hub@users.noreply.github.com>
2025-09-30 11:41:24 +0100

    feat: auth and session

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,6 +1,10 @@
+import { auth } from "@/lib/auth";
+import Image from "next/image";
 import Link from "next/link";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -21,12 +25,29 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href="/account"
+              className="flex items-center gap-4 hover:text-accent-400 transition-colors"
+            >
+              <Image
+                width={32}
+                height={32}
+                src={session.user.image}
+                className="rounded-full"
+                alt={session.user.image}
+                referrerPolicy="no-referrer"
+              />
+              Guest area
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
--- components/Reservation.js
+++ components/Reservation.js
@@ -2,12 +2,16 @@ import React from "react";
 import DateSelector from "./DateSelector";
 import ReservationForm from "./ReservationForm";
 import { getBookedDatesByCabinId, getSettings } from "@/lib/data-service";
+import { auth } from "@/lib/auth";
+import LoginMessage from "./LoginMessage";
 
 export default async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+
+  const session = await auth();
 
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
@@ -16,7 +20,11 @@ export default async function Reservation({ cabin }) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
